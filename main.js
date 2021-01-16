eb769025b8528fb5104f70f23d3882195d6d36d0
Justin Nguyen <70992422+jnvtnguyen@users.noreply.github.com>
2021-01-16 14:54:48 -0800

    f

---
+++ main.js
@@ -0,0 +1,47 @@
+(function() {
+    "use strict";
+    var app = angular.module("MainApp", ["ngRoute"]);
+    app.config(["$routeProvider", "$locationProvider",
+        function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when("/", {
+                    templateUrl: "home.html",
+                    title: "Home - Andre Nguyen"
+                })
+                .when("/videos", {
+                    templateUrl: "videos.html",
+                    title: "Videos - Andre Nguyen"
+                })
+                .when("/about", {
+                    templateUrl: "about.html",
+                    title: "About - Andre Nguyen"
+                })
+                .when("/contact", {
+                    templateUrl: "contact.html",
+                    title: "Contact - Andre Nguyen"
+                });
+            $locationProvider.html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        }]);
+
+    app.controller("MainController", [
+        "$scope",
+        "$route",
+        "$location",
+        mainController
+    ]);
+
+    function mainController($scope, $route, $location) {
+        var vm = this;
+        
+        let path = localStorage.getItem("path");
+        $location.path(path);
+        $scope.$on("$routeChangeSuccess", function handleRouteChangeSuccessEvent(evt) {
+            vm.view = {
+                title: $route.current.title
+            }
+        });
+    }
+}());
